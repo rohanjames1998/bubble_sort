9711e3a07944a17d4a46aee4b57764b2e5f746c7
Rohan James <rohanjames9282@gmail.com>
2022-08-11 10:27:39 +0530

    Remove debugger

--- bub_sort.rb
+++ bub_sort.rb
@@ -1,6 +1,3 @@
-
-
-
 def bubble_sort(unsorted_array)
     #sort checker makes stops our sort function when we have sorted the array
     sort_checker = unsorted_array.length - 1
@@ -8,15 +5,16 @@ def bubble_sort(unsorted_array)
     
     loop do
         # Since after n number of iterations the last element is sorted we don't need to check for it
-        # binding.pry
+        # next keyword is to avoid comparing last element with nil value
         if index == sort_checker
             index = 0
             sort_checker -= 1
             next
         end
-            #swapping elements if they are out of order
-            element_before = unsorted_array[index]
-            element_after = unsorted_array[index + 1]
+        # Assigning elements variables so we can avoid duplication when swapping them
+        element_before = unsorted_array[index]
+        element_after = unsorted_array[index + 1]
+        #swapping elements if they are out of order
         if element_before > element_after
             unsorted_array[index] = element_after
             unsorted_array[index + 1] = element_before
