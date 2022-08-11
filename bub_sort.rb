e6f9b3b7e38a693d7b298e42f8aa9a87ec5a52b5
Rohan James <rohanjames9282@gmail.com>
2022-08-11 10:31:14 +0530

    Fix comments

--- bub_sort.rb
+++ bub_sort.rb
@@ -1,20 +1,20 @@
 def bubble_sort(unsorted_array)
-    #sort checker stops our sort function when we have sorted the array
+    # Sort checker stops our sort function when we have sorted the array.
     sort_checker = unsorted_array.length - 1
     index = 0
     
     loop do
-        # Since after n number of iterations the last element is sorted we don't need to check for it thats why we diminish it by 1 when we reach the end of the array
-        # next keyword is to avoid comparing last element with nil value
+        # Since after n number of iterations the last element is sorted we don't need to check for it thats why we diminish it by 1 when we reach the end of the array.
+        # Next keyword is to avoid comparing last element with nil value.
         if index == sort_checker
             index = 0
             sort_checker -= 1
             next
         end
-        # Assigning elements variables so we can avoid duplication when swapping them
+        # Assigning elements variables so we can avoid duplication when we swap them.
         element_before = unsorted_array[index]
         element_after = unsorted_array[index + 1]
-        #swapping elements if they are out of order
+        # Swapping elements if they are out of order.
         if element_before > element_after
             unsorted_array[index] = element_after
             unsorted_array[index + 1] = element_before
