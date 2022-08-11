526eda5ae0cbe56d9269ed9ebf593d3d5a4031dd
Rohan James <rohanjames9282@gmail.com>
2022-08-11 10:22:11 +0530

    Assign variables to elements that need comparision

--- bub_sort.rb
+++ bub_sort.rb
@@ -29,21 +29,24 @@ def bubble_sort(unsorted_array)
     
     loop do
         # Since after n number of iterations the last element is sorted we don't need to check for it
+        # binding.pry
         if index == sort_checker
             index = 0
             sort_checker -= 1
             next
+        end
             #swapping elements if they are out of order
-        elsif unsorted_array[index] > unsorted_array[index + 1]
-            unsorted_array[index + 1] = unsorted_array[index]
-            unsorted_array[index] = unsorted_array[index + 1] 
+            element_before = unsorted_array[index]
+            element_after = unsorted_array[index + 1]
+        if element_before > element_after
+            unsorted_array[index] = element_after
+            unsorted_array[index + 1] = element_before
             index += 1
         else 
             index += 1
             
         end
-        # binding.pry
-        break if sort_checker <= 0
+        break if sort_checker < 0
     end
     unsorted_array
 end
